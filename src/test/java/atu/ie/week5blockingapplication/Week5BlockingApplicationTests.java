3e1050afeb1c8595ee64bfe10e48645f37345bb6
Olamide Aderogba <G00401971@atu.ie>
2023-10-18 14:46:37 +0100

    Created Blocking Application and added OpenFeign to project.

---
+++ src/test/java/atu/ie/week5blockingapplication/Week5BlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.week5blockingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
