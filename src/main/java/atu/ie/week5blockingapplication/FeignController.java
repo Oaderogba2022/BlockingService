892033804fffe76fc155cbed7e857d45e77fbebb
Olamide Aderogba <G00401971@atu.ie>
2023-10-31 21:10:00 +0000

    Retrieving a list of ToDos from the JsonPlaceHolder website

--- src/main/java/atu/ie/week5blockingapplication/FeignController.java
+++ src/main/java/atu/ie/week5blockingapplication/FeignController.java
@@ -1,10 +1,9 @@
 package atu.ie.week5blockingapplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+
 
 @RestController
 public class FeignController {
@@ -15,25 +14,21 @@ public class FeignController {
         this.feignService = feignService;
     }
 
-    @GetMapping("/feign")
-    public String getFeignData() throws Exception {
-        long startTime = System.currentTimeMillis();
 
-        List<CompletableFuture<TodoResponse>> futures = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++) {
-            CompletableFuture<TodoResponse> future = CompletableFuture.supplyAsync(() -> feignService.fetchData());
-            futures.add(future);
-        }
-
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-
-        allOf.get();
-        long endTime = System.currentTimeMillis();
-
-        return "Total execution time: " + (endTime - startTime) + "ms";
-
-
-
+    @GetMapping("/todos")
+    public List<TodoResponse> getAllTodos() {
+        return feignService.fetchAllTodoes();
     }
+
+    @GetMapping("/todos/{id}")
+    public TodoResponse getTodoById(@PathVariable("id") int id) {
+        return feignService.fetchTodoById(id);
+    }
+
+    @GetMapping("/todos/json/{id}")
+    public TodoResponse getTodoJsonById(@PathVariable("id") int id) {
+        return feignService.fetchTodoById(id);
+    }
+
+
 }
--- src/main/java/atu/ie/week5blockingapplication/TodoClient.java
+++ src/main/java/atu/ie/week5blockingapplication/TodoClient.java
@@ -1,9 +1,15 @@
 package atu.ie.week5blockingapplication;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "jsonplaceholder", url = "http://jsonplaceholder.typicode.com")
 public interface TodoClient {
     @GetMapping("/todos/1")
-    TodoResponse fetchData();
+    List<TodoResponse> fetchTodoes();
+
+    @GetMapping("/todos/{id}")
+    TodoResponse fetchTodoId(@PathVariable("id")int id);
 }
